950da6b7a0104df46f8872eb84d172496a2fad29
Vijay09261 <vp9261@gmail.com>
2024-10-24 14:15:25 +0530

    output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,8 +1,3 @@
-output "ansible_instance_ip" {
-  value = aws_instance.ansible_instance.public_ip
-  description = "The public IP address of the Ansible instance"
-}
-
 output "host_instance_ip" {
   value = aws_instance.host_instance.public_ip
   description = "The public IP address of the Host instance"
